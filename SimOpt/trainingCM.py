e2c96b6e344346c1c81b81b675918c799d9a833a
dalial89 <77731560+dalial89@users.noreply.github.com>
2025-06-15 15:14:12 +0200

    Update trainingCM.py

--- SimOpt/trainingCM.py
+++ SimOpt/trainingCM.py
@@ -182,7 +182,7 @@ def main():
 #TRAIN THE DEFINITIVE MODEL
 	n_eval_episodes = 50
 	eval_interval = 1000 
-	total_timesteps = 100000
+	total_timesteps = 500000
 	
 	sim_env = Monitor(gym.make('CustomHopper-source-v0'))
 	masses = sim_env.get_parameters()
