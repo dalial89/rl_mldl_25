bc637f1ca77986747c51120a5b4e2c6c948c91dc
Emanuele-Elias <s344489@studenti.polito.it>
2025-06-21 15:21:43 +0200

    ok

--- tuning/PPO_tuning.py
+++ tuning/PPO_tuning.py
@@ -41,7 +41,7 @@ def main():
 
     # 4) Hyperparameter grid
     param_grid = {
-        "gamma":         [0.99],
+        "gamma":         [0.95,0.98, 0.99],
         "learning_rate": [lr_schedule, 1e-3, 3e-4,1e-6, 1e-8]
 
     }
